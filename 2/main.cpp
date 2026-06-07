de8c5e2caf5746b410a0a0c18a5a0845d06571e8
unknown <jan.faferek@student.put.poznan.pl>
2026-06-07 14:10:26 +0200

    test5

--- 2/main.cpp
+++ 2/main.cpp
@@ -3,29 +3,3 @@
 using namespace std;
 
 
-class L
-{
-public:
-    int liczba;
-    L operator+(const L &licz)const;
-};
-
-L L::operator+(const L &licz)const
-{
-    L wynik;
-    wynik.liczba = liczba + licz.liczba;
-    return wynik;
-}
-
-
-int main()
-{
-    L a;
-    L b;
-    a.liczba = 5;
-    b.liczba = 11;
-    L c;
-    c = a+b;
-    cout<<c.liczba<<endl;
-    return 0;
-}
